5201906d9ed3ae7e42b7c273e203677b9159ff1e
Grant Moore <chinchalinchin@gmail.com>
2022-07-19 13:12:53 -0400

    renaming asset files for consistency, setting up spritesheet configuration file

--- src/onta/settings.py
+++ src/onta/settings.py
@@ -2,13 +2,12 @@ import os
 
 
 APP_DIR = os.path.dirname(os.path.abspath(__file__))
-"""Folder containing the root module of the project"""
+"""Directory containing the root module of the project"""
 
-PROJECT_DIR = os.path.dirname(APP_DIR)
-"""Folder containing the project source"""
+SRC_DIR = os.path.dirname(APP_DIR)
+"""Directory containing the project source"""
 
-
-ASSET_DIR = os.path.join(APP_DIR, 'data', 'assets')
+ASSET_DIR = os.path.join(SRC_DIR, 'data', 'assets')
 """Directory containing application assets."""
 
 CONF_DIR = os.path.join(ASSET_DIR, 'conf')
@@ -16,3 +15,5 @@ CONF_DIR = os.path.join(ASSET_DIR, 'conf')
 
 SPRITE_DIR = os.path.join(ASSET_DIR, 'sprites')
 """Diretory containing spritesheets"""
+
+SHEET_SIZE = (832, 1344)
