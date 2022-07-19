f40fe5ddc4181f27b90138ba80b1b310369bcfab
Grant Moore <chinchalinchin@gmail.com>
2022-07-19 15:09:55 -0400

    add script to install package, add skeleton for basic gui widget and display

--- src/onta/main.py
+++ src/onta/main.py
@@ -1,5 +1,19 @@
+import sys
+from PySide6 import QtWidgets, QtGui
+
+import onta.view as view
+
 def be():
-    pass
+    app = QtWidgets.QApplication([])
+    game = view.view()
+    game.resize(800, 800)
+    center = QtGui.QScreen.availableGeometry(
+        QtWidgets.QApplication.primaryScreen()).center()
+    geo = game.frameGeometry()
+    geo.moveCenter(center)
+    game.move(geo.topLeft())
+    game.show()
+    sys.exit(app.exec_())
 
 if __name__=="__main__":
     be()
---
+++ src/onta/view.py
@@ -0,0 +1,4 @@
+from PySide6 import QtWidgets
+
+def view():
+    return QtWidgets.QWidget()
