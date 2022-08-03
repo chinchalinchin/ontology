7bc21b805af9925a299bd2136ab350d0f5193ab1
Grant Moore <chinchalinchin@gmail.com>
2022-08-03 07:26:45 -0400

    general formatting

--- src/onta/engine/calculator.py
+++ src/onta/engine/calculator.py
@@ -1,17 +1,28 @@
 import math
 
 
-def projection(angle:float = 45) -> float:
-    return math.cos(angle*math.pi/180), math.sin(angle*math.pi/180)
+def projection(
+    angle:float = 45
+) -> tuple:
+    return (
+        math.cos(angle*math.pi/180), 
+        math.sin(angle*math.pi/180)
+    )
 
 
-def distance(a: tuple, b: tuple) -> float:
+def distance(
+    a: tuple, 
+    b: tuple
+) -> float:
     dx = a[0] - b[0]
     dy = a[1] - b[1]
     return math.sqrt(dx*dx + dy*dy)
 
 
-def intersection(rect_a: tuple, rect_b: tuple) -> bool:
+def intersection(
+    rect_a: tuple, 
+    rect_b: tuple
+) -> bool:
     """Determines if two rectangles intersect. Rectangles are defined by the coordinate of the upper-left corner (as viewed in screen units, where the down is the positive y-direction), and its dimensions (i.e., width and height)
 
 
@@ -23,25 +34,44 @@ def intersection(rect_a: tuple, rect_b: tuple) -> bool:
     :rtype: bool
     """    
     
+    dims = [
+        rect_a[2], 
+        rect_a[3], 
+        rect_b[2], 
+        rect_b[3]
+    ]
+
     # verify rectangles have area
-    if 0 in [rect_a[2], rect_a[3], rect_b[2], rect_b[3]]:
+    if 0 in dims:
         return False
 
-    bottom_corner_a = (rect_a[0] + rect_a[2], rect_a[1] + rect_a[3])
-    bottom_corner_b = (rect_b[0] + rect_b[2], rect_b[1] + rect_b[3])
+    bottom_corner_a = (
+        rect_a[0] + rect_a[2], 
+        rect_a[1] + rect_a[3]
+    )
+    bottom_corner_b = (
+        rect_b[0] + rect_b[2], 
+        rect_b[1] + rect_b[3]
+    )
 
     # verify rectangles do not overlap in horizontal direction
-    if bottom_corner_a[0] < rect_b[0] or rect_a[0] > bottom_corner_b[0]:
+    if bottom_corner_a[0] < rect_b[0] or \
+        rect_a[0] > bottom_corner_b[0]:
         return False
 
     # verify rectangles do not overlap in vertical direction
-    if bottom_corner_a[1] < rect_b[1] or rect_a[1] > bottom_corner_b[1]:
+    if bottom_corner_a[1] < rect_b[1] or \
+        rect_a[1] > bottom_corner_b[1]:
         return False
 
     return True
 
 
-def scale(point: tuple, factor: tuple, units: str = 'absolute') -> tuple:    
+def scale(
+    point: tuple, 
+    factor: tuple, 
+    units: str = 'absolute'
+) -> tuple:    
     """Scales a coordinate _(x,y)_ by the tile dimensions if the units match, otherwise returns point unaltered.
 
     :param point: _(x,y))_
